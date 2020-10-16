7d282efd1bd76f8d70c2e64d36c3891afd0aac47
雨泣 <panjunyu10@163.com>
2020-10-16 09:14:49 +0800

    tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img src="~assets/image/icon_xjfb_tabbar_shouye_no.png">
+    <!-- <img src="~assets/image/icon_xjfb_tabbar_shouye_no.png"> -->
     <router-view></router-view>
-    <!-- <tabbar-main/> -->
+    <tabbar-main></tabbar-main>
   </div>
 </template>
 
 <script>
-// import TabbarMain from "@/components/content/TabbarMain.vue"
+import TabbarMain from "components/content/TabbarMain.vue"
 export default {
   name: 'App',
   components:{
-    // TabbarMain
+    TabbarMain
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,34 +2,39 @@ import Vue from "vue"
 import VueRouter from "vue-router"
 Vue.use(VueRouter)
 
-// const Home = () => import('../views/mine/Mine.vue')
-// const Mine = ()=> import("../views/mine/Mine.vue")
-// const Category = ()=> import("../views/category/category.vue")
-// const Cart = ()=> import("../views/cart/Cart.vue")
+const Home = () => import('../views/home/Home.vue')
+const Mine = ()=> import("../views/mine/Mine.vue")
+const Category = ()=> import("../views/category/Category.vue")
+const Cart = ()=> import("../views/cart/Cart.vue")
 
 // console.log(Mine,'mine' )
 
+// import Home from "../views/home/Home.vue"
+// import Mine from "../views/mine/Mine.vue"
+// import Cart from "../views/cart/Cart.vue"
+// import Category from "../views/category/Category.vue"
+
 const routes=[
-    // {
-    //     path:"/",
-    //     redirect:"/home"
-    //   },
-      // {
-      //   path:"/home",
-      //   component:Home
-      // },
-    //   {
-    //     path:"/mine",
-    //     component:Mine
-    //   },
-    //   {
-    //     path:"/cart",
-    //     component:Cart
-    //   },
-    //   {
-    //     path:"/category",
-    //     component:Category
-    //   }
+    {
+        path:"/",
+        redirect:"/home"
+      },
+      {
+        path:"/home",
+        component:Home
+      },
+      {
+        path:"/mine",
+        component:Mine
+      },
+      {
+        path:"/cart",
+        component:Cart
+      },
+      {
+        path:"/category",
+        component:Category
+      }
 ]
 
 const router = new VueRouter({
